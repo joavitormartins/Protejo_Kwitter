9d0d7e37b8cc2338f048c5d6029b2cf4f61e173b
JoãoVitor <99548321+joavitormartins@users.noreply.github.com>
2023-03-11 12:23:27 -0300

    Update kwitter_room.js

--- kwitter_room.js
+++ kwitter_room.js
@@ -51,5 +51,5 @@ getData()
     function redirectToRoomName(name){
         console.log(name);
         localStorage.setItem("room_name",name);
-        window.location = "kwitter_page.html"
+        window.location = "./kwitter_page.html"
     }
